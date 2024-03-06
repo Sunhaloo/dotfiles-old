a3b9f277c617d9d3b3a16b0513cd64521bb47e7e
Sunhaloo <azmaan151003@gmail.com>
2024-03-06 12:54:08 +0400

    Good to Go (for now)

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    tag = "v0.9.1",
+    opts = {
+      ensure_installed = {
+        "python",
+        "css",
+        "gitignore",
+        "graphql",
+        "http",
+        "json",
+        "vim",
+        "lua",
+        "yaml",
+      },
+      query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = { "BufWrite", "CursorHold" },
+      },
+    },
+  },
+}
