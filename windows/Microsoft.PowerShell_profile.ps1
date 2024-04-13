0963c73e63a8781d69547373fe1cb74ccb74e2c9
Sunhaloo <azmaan151003@gmail.com>
2024-04-13 22:29:30 +0400

    added more themes to oh-my-posh themes

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,3 @@
-# My Oh-My-Posh Configuration File
-
 oh-my-posh init pwsh | Invoke-Expression
 
 # robbyrussell
@@ -8,3 +6,17 @@ oh-my-posh init pwsh --config 'C:\Users\Azmaan\AppData\Local\Programs\oh-my-posh
 # gruvbox
 # oh-my-posh init pwsh --config 'C:\Users\Azmaan\AppData\Local\Programs\oh-my-posh\themes\gruvbox.omp.json' | Invoke-Expression
 
+# amro
+# oh-my-posh init pwsh --config 'C:\Users\Azmaan\AppData\Local\Programs\oh-my-posh\themes\amro.omp.json' | Invoke-Expression
+
+# Bubbles
+# oh-my-posh init pwsh --config 'C:\Users\Azmaan\AppData\Local\Programs\oh-my-posh\themes\bubbles.omp.json' | Invoke-Expression
+
+# json
+# oh-my-posh init pwsh --config 'C:\Users\Azmaan\AppData\Local\Programs\oh-my-posh\themes\json.omp.json' | Invoke-Expression
+
+# star
+# oh-my-posh init pwsh --config 'C:\Users\Azmaan\AppData\Local\Programs\oh-my-posh\themes\star.omp.json' | Invoke-Expression
+
+# takuya
+# oh-my-posh init pwsh --config 'C:\Users\Azmaan\AppData\Local\Programs\oh-my-posh\themes\takuya.omp.json' | Invoke-Expression
