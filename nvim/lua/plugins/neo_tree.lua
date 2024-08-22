e82e1caf6d17da108fe131ca8381814b570e7833
Sunhaloo <azmaan151003@gmail.com>
2024-08-22 07:58:58 +0400

    Happy with the way it is for now!

--- nvim/lua/plugins/neo_tree.lua
+++ nvim/lua/plugins/neo_tree.lua
@@ -11,6 +11,34 @@ return {
         keys = {
             {"<leader>e", ":Neotree toggle left<CR>", silent = true, desc="FileExplorer Left"},
             {"<leader>f", ":Neotree toggle float<CR>", silent = true, desc="FileExplorer Float"}
-        }
+        },
+        config = function()
+            require("neo-tree").setup({
+                popup_border_style = "rounded",
+                enable_git_status = true,
+                sort_case_insensitive = false,
+                filesystem = {
+                    filtered_items = {
+                        -- visible = true,
+                        -- hide_dotfiles = false,
+                        -- hide_gitignored = false,
+                        sort_order = "asc",
+                        always_show = {
+                            ".git",
+                            ".obsidian"
+                        },
+                    },
+                    group_empty_dirs = false,
+                },
+                default_component_configs = {
+                    icon = {
+                        folder_closed = "",
+                        folder_open = "",
+                        folder_empty = "󰉖",
+                        folder_empty_open = "󰷏"
+                    }
+                }
+            })
+        end,
     }
 }
