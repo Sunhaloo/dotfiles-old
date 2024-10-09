ef311c495db203864b346bf7623094f5b38e44b9
Sunhaloo <azmaan151003@gmail.com>
2024-10-09 16:43:51 +0400

    Finally the colorizer plugin is working
    I used the LunarVim version of colorizer... Even though that colorizer
    plugins is basically the same thing ( by the same author )...

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -182,15 +182,29 @@ return {
             todo_comments.setup()
         end,
     },
-    -- well, colorizer
     {
-        'echasnovski/mini.hipatterns',
-        version = false,
+      "norcalli/nvim-colorizer.lua",
         config = function()
-            require("mini.hipatterns").setup()
-        end
-
+          require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
+              RGB = true, -- #RGB hex codes
+              RRGGBB = true, -- #RRGGBB hex codes
+              RRGGBBAA = true, -- #RRGGBBAA hex codes
+              rgb_fn = true, -- CSS rgb() and rgba() functions
+              hsl_fn = true, -- CSS hsl() and hsla() functions
+              css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+              css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+              })
+      end,
     },
+    -- well, colorizer
+    -- {
+    --     'echasnovski/mini.hipatterns',
+    --     version = false,
+    --     config = function()
+    --         require("mini.hipatterns").setup()
+    --     end
+    --
+    -- },
     -- caret animation
     {
         "echasnovski/mini.animate",
