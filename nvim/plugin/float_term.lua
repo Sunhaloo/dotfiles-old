152d44429f65cdb6a396ca61b55787d382119fb9
Sunhaloo <azmaan151003@gmail.com>
2024-12-29 19:05:53 +0400

    Update: Completed the Floating Terminal... Will now Update Horizontal + Vertical

--- nvim/plugin/float_term.lua
+++ nvim/plugin/float_term.lua
@@ -24,9 +24,12 @@ local function create_floating_window(opts)
     -- create a buffer in the window
     local buf = nil
     -- check if there is anything in that buffer
-    if vim.api.nvim_buf_is_valid(opt.buf) then
+    if vim.api.nvim_buf_is_valid(opts.buf) then
         -- then keep the contents of the buffer
+        buf = opts.buf
+    -- if there is nothing in the buffer
     else
+        -- create the buffer
         buf = vim.api.nvim_create_buf(false, true) -- No file, scratch buffer
     end
 
@@ -46,16 +49,29 @@ local function create_floating_window(opts)
     return { buf = buf, win = win }
 end
 
--- create a Neovim user-command that can be run in the command line ( similar to auto-command )
-vim.api.nvim_create_user_command("FloatingTerminal", function ()
+-- create a function for toggle-able terminal
+local toggle_term = function ()
     -- create a new floating window if not created
     if not vim.api.nvim_win_is_valid(state.floating.win) then
         -- create the window and pass the previous buffer
         -- I think if we don't pass the previous buffer, it will just a "new" one
         state.floating = create_floating_window { buf = state.floating.buf }
+        -- check if the current buffer created is a "terminal"
+        -- NOTE: `~=` is "Not Equal To" ( `!=` this seems more intuitive )
+        if vim.bo[state.floating.buf].buftype ~= "terminal" then
+            -- create the terminal in that buffer by calling the command
+            vim.cmd.term()
+            vim.cmd("startinsert")
+        end
     -- if the window is already open ==> hide it
     else
         -- hide the window
         vim.api.nvim_win_hide(state.floating.win)
     end
-end, {})
+end
+
+-- create a Neovim user-command that can be run in the command line ( similar to auto-command )
+vim.api.nvim_create_user_command("FloatingTerminal", toggle_term, {})
+
+-- create the default 'toggleterm.nvim' keymaps for terminal
+vim.keymap.set({'n', 't'}, "<C-/>", "<Cmd>FloatingTerminal<CR>")
