372a48e5a119afd04a0f60ee4e29dfa57ee5c19f
Sunhaloo <azmaan151003@gmail.com>
2024-12-30 14:41:29 +0400

    Update: Added mini.pairs
    I even added a custom autopairs for '<' and '>'

--- nvim/lua/plugins/mini_related.lua
+++ nvim/lua/plugins/mini_related.lua
@@ -36,5 +36,22 @@ return {
                 }
             })
         end,
+    },
+    {
+        -- choosing 'mini.pairs' and testing it ( over 'nvim-autopairs' )
+        "echasnovski/mini.pairs",
+        version = false,
+        -- start on when in Insert Mode
+        event = "InsertEnter",
+        -- configuration for 'mini.pairs'
+        config = function()
+            -- enable the autopairs plugin
+            require("mini.pairs").setup({
+                mappings = {
+                    ['<'] = { action = 'open', pair = '<>', neigh_pattern = '[^\\].' },
+                    ['>'] = { action = 'close', pair = '<>', neigh_pattern = '[^\\].' },
+                }
+            })
+        end,
     }
 }
--- nvim/plugin/terminal.lua
+++ nvim/plugin/terminal.lua
@@ -51,7 +51,3 @@ end,
         desc = "Open Vertical Terminal"
     }
 )
-
-
--- return "nothing" ==> suppresses lazy's annoying 'spec' message
--- return {}
