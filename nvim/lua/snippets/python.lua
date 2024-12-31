f787183ecff68a3f2205adf20d56943f50f45cf0
Sunhaloo <azmaan151003@gmail.com>
2024-12-31 17:36:09 +0400

    Update: LuaSnip!!!
    - Change snippets.lua to luasnip.lua
    - Added custom snippets directory
    - Added lua.lua and python.lua ( + configured them )
    
    But we still need to configure for other... Right now I am very happy

---
+++ nvim/lua/snippets/python.lua
@@ -0,0 +1,22 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+
+return {
+    -- "main" function + source 'main' in Python
+    s("main", {
+        t({
+            "def main():",
+            "   print(\"Hello World\")",
+        }),
+        -- drop the cursor in here ( unfortunately still in INSERT Mode )
+        i(1),
+        t({
+            "",
+            "",
+            "if __name__ == '__main__':",
+            "   main()"
+        })
+    })
+}
