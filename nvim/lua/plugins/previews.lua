17b9a47d39edd3a0e30faf0043d433c23a001b04
Sunhaloo <azmaan151003@gmail.com>
2025-04-30 14:00:29 +0400

    Update: Cleaned Up Some Code

--- nvim/lua/plugins/previews.lua
+++ nvim/lua/plugins/previews.lua
@@ -4,19 +4,29 @@ return {
         "sylvanfranklin/omni-preview.nvim",
         -- dependencies to install with 'omni-preview'
         dependencies = {
-            -- markdown previewer
-            { "toppair/peek.nvim", lazy = true, build = "deno task --quiet build:fast" }
+            {
+                -- markdown previewer
+                "toppair/peek.nvim",
+                -- plugin is NOT loaded in immediately during startup
+                lazy = true,
+                -- on what filetype to act
+                ft = "markdown",
+                -- make sure that you have 'deno' installed on your system
+                build = "deno task --quiet build:fast"
+            },
         },
         -- configuration for 'omni-preview'
         config = function()
             require("omni-preview").setup()
+
             -- configuration for 'peek' markdown previewer
             require("peek").setup({
                 -- change default preview from 'webview' to 'browser'
                 app = "browser"
             })
         end,
-        -- customise the keymappings
+
+        -- customise the keymappings ( omni-preview )
         keys = {
             { "<leader>Op", "<Cmd>OmniPreview start<CR>", desc = "Start Omni Preview" },
             { "<leader>op", "<Cmd>OmniPreview stop<CR>",  desc = "Stop Omni Preview" }
