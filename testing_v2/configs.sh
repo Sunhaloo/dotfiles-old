c5f028244ddfe928d7cac63b1879dc694f369065
Sunhaloo <azmaan151003@gmail.com>
2025-07-08 16:41:20 +0400

    Update: Arrived at Actually Moving the Config Files

--- testing_v2/configs.sh
+++ testing_v2/configs.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 # define the dotfiles repository URL
-REPO_URL="https://github.com/Sunhaloo/dotfiles.git"
-# REPO_URL="https://github.com/Sunhaloo/doits.git"
+# REPO_URL="https://github.com/Sunhaloo/dotfiles.git"
+REPO_URL="https://github.com/Sunhaloo/doits.git"
 # define the dotfiles folder name
-DIR_NAME="dotfiles"
-# DIR_NAME="actual_repo"
+# DIR_NAME="dotfiles"
+DIR_NAME="actual_repo"
 # define my directory where I keep all my repositories
-REPO_DIR="$HOME/GitHub/"
-# REPO_DIR="$HOME/gitshit/"
+# REPO_DIR="$HOME/GitHub/"
+REPO_DIR="$HOME/gitshit/"
 # define the configuration directory
-CONFIG_DIR="$HOME/.config/"
-# CONFIG_DIR="$HOME/.shitter/"
+# CONFIG_DIR="$HOME/.config/"
+CONFIG_DIR="$HOME/.shitter/"
 
 # change current working directory to home directory
 cd $HOME
@@ -23,8 +23,8 @@ if [[ -d "$REPO_DIR$DIR_NAME" ]]; then
     # meaning that the 'dotfiles' repository already exists
     printf "== Dotfiles Repository Already Exists! ==\n\n"
 
-    printf "\nThe Repository Is Located At: "
-    find -type d -name "$REPO_DIR$DIR_NAME" 2> /dev/null
+    # show the user where the repository has been installed
+    printf "-- Repository Located At: %s --\n\n" "$(find "$HOME" -type d -name "$DIR_NAME" 2>/dev/null)"
 
     printf "== Checking Required Directories ==\n\n"
 
@@ -67,6 +67,48 @@ else
     git clone "$REPO_URL" "$REPO_DIR$DIR_NAME"
 
     # show the user where the repository has been installed
-    printf "\nThe Repository Is Located At: %s" find -type d -name "$REPO_DIR$DIR_NAME" 2> /dev/null
+    printf "\n-- Repository Located At: %s --\n\n" "$(find "$HOME" -type d -name "$DIR_NAME" 2>/dev/null)"
 
 fi
+
+# WARNING: need to complete this part... Going to for supper :)
+
+# check if the cloning of the repository was successful
+if [[ $? -eq 0 ]]; then
+    # meaning that return status is '0' ==> clone successful
+    # therefore, move each folder into their respective positions
+    printf "+      Moving Configurations!!!       +\n"
+
+    echo
+
+    if [[ -d "$HOME/{Obsidian,OBS\ Studio,Screenshots,Wallpapers}" ]]; then
+        printf "+      Creating Home Folders!         +\n\n"
+        # create the all the required folders that I need
+        mkdir ~/{Obsidian,OBS\ Studio,Screenshots,Wallpapers}
+
+    else
+        # output appropriate message
+        printf "+\tHome Folders Exists!\t      +\n\n"
+    fi
+
+    printf "+\t   Moving Folders!\t      +\n\n"
+
+    # move the required configuration files
+    # NOTE: remember to add starship to configuration files
+    cp -r $HOME/GitHub/dotfiles/{hypr,waybar,kitty,nvim,rofi} ~/.config
+    cp -r $HOME/GitHub/dotfiles/{.zshrc,.tmux.conf} $HOME
+
+    printf "+      Moving Folders Completed!      +\n\n"
+
+
+# meaning that the clone was not successful
+else
+    # output appropriate message
+    echo
+    printf "+      WARNING: Cloned Failed!!!      +\n"
+
+    # return to the shell with exit status code '1'
+    # exit 1
+    return 1
+fi
+
