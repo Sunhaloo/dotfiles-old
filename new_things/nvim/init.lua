29ce2620617a2d3e653b12717930920811ce881b
Sunhaloo <azmaan151003@gmail.com>
2025-07-25 10:37:37 +0400

    Update: Starting Neovim Configuration

---
+++ new_things/nvim/init.lua
@@ -0,0 +1,8 @@
+-- "import" the keymaps configuration file
+require("config.keymaps")
+-- "import" the options configuration file
+require("config.options")
+-- "import" the lazy configuration file
+require("config.lazy")
+-- "import" the auto-command file
+require("config.autocmd")
