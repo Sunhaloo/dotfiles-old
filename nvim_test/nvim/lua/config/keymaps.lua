970b32838e62d7938e52b2321b71c3dc5b2a2745
Sunhaloo <azmaan151003@gmail.com>
2025-08-24 00:39:51 +0400

    Update: Actually setting up LazyVim

--- nvim_test/nvim/lua/config/keymaps.lua
+++ nvim_test/nvim/lua/config/keymaps.lua
@@ -1,70 +1,23 @@
--- variable that will truncate `vim.keymap`
-local key = vim.keymap
+local custom_opts = { noremap = true, silent = true }
 
--- variable that will hold options for `noremap` and `silent`
--- `noremap = true` ==> no recursion for the mapping.
--- `silent = true` ==> no need to show "keys" when pressing keys in status line
-local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "<C-a>", "gg<S-v>G", { desc = "Select All" })
 
--- NOTE: using `<Cmd>` means that you need to use `<CR>
+vim.keymap.set("n", "sh", "<cmd>split<cr>", { desc = "Horizontal Split ( Same File )" })
+vim.keymap.set("n", "sv", "<cmd>vsplit<cr>", { desc = "Vertical Split ( Same File )" })
+vim.keymap.set("n", "<leader>sh", "<cmd>new<cr>", { desc = "Horizontal Split" })
+vim.keymap.set("n", "<leader>sv", "<cmd>vnew<cr>", { desc = "Vertical Split" })
 
--- == Reload and Package Management ==
+vim.keymap.set("n", "te", "<cmd>tabnew<cr>", { desc = "New Tab" })
+vim.keymap.set("n", "<tab>", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+vim.keymap.set("n", "<s-tab>", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
 
--- reload the current file
-key.set("n", "<leader>R", "<Cmd>source %<CR>", { desc = "Reload Neovim" })
--- open 'lazy' "start" window
-key.set("n", "<leader>L", "<Cmd>Lazy<CR>", { desc = "Lazy Plugin Manager" })
+vim.keymap.set("n", "+", "<C-a>", { desc = "Increment Number" })
+vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement Number" })
 
--- == Quality of Life ==
+vim.keymap.set("n", "<leader>tr", ":s/", { desc = "Replace One Occurrences" })
+vim.keymap.set("n", "<leader>tR", ":%s/", { desc = "Replace All Occurrences" })
 
--- remap <Esc> to <jk>
-key.set("i", "jk", "<ESC>", opts, { desc = "Exit Insert Mode" })
--- select all within file
-key.set("n", "<C-a>", "gg<S-v>G", { desc = "Select All" })
--- copy to system clipboard
-key.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copy TO System Clipboard" })
--- paste from system clipboard
-key.set({ "n", "v" }, "<leader>p", [["+p]], { desc = "Paste FROM System Clipboard" })
--- search and replace one occurrence of that word
-key.set("n", "<leader>sh", ":s/", { desc = "Search + Replace ( One )" })
--- search and replace all occurrences of that word
-key.set("n", "<leader>sH", ":%s/", { desc = "Search + Replace ( All )" })
--- increment number
-key.set("n", "+", "<C-a>", { desc = "Increment Number" })
--- decrement number
-key.set("n", "-", "<C-x>", { desc = "Decrement Number" })
-
--- my weird fucking keymappings ( My QOL Improvement )
-key.set("i", "<C-h>", "<Left>", opts)
-key.set("i", "<C-l>", "<Right>", opts)
-key.set("i", "<C-j>", "<Down>", opts)
-key.set("i", "<C-k>", "<Up>", opts)
-
--- == Coding / Writing ==
-
--- move lines / blocks of text up / down
-key.set("v", "<A-k>", ":m '<-2<CR>gv=gv", opts, { desc = "Move Lines / Blocks of Text Up" })
-key.set("v", "<A-j>", ":m '>+1<CR>gv=gv", opts, { desc = "Move Lines / Blocks of Text Down" })
--- paste even when in visual mode
-key.set("v", "p", '"_dP', opts, { desc = "Paste in Visual Mode" })
--- stay in visual ( indent ) mode when indenting lines / block of text
-key.set("v", "<", "<gv", opts, { desc = "Stay in Visual Mode ( Indent In )" })
-key.set("v", ">", ">gv", opts, { desc = "Stay in Visual Mode ( Indent Out )" })
-
--- == Tabs - Buffers - Splits ==
-
--- create a new tab
-key.set("n", "te", "<Cmd>tabnew<CR>", { desc = "Open New Tab" })
--- switch to next tab
-key.set("n", "<Tab>", "<Cmd>tabnext<CR>", { desc = "Switch to Next Tab" })
--- switch to previous tab
-key.set("n", "<S-Tab>", "<Cmd>tabprev<CR>", { desc = "Switch to Previous Tab" })
--- close tab without saving
-key.set("n", "<leader>Q", "<Cmd>q!<CR>", { desc = "Close Tab Without Saving" })
--- save and close tab
-key.set("n", "<leader>q", "<Cmd>wq<CR>", { desc = "Save and Close Tab" })
-
--- split window horizontally with same buffer
-key.set("n", "ss", "<Cmd>split<CR>", opts, { desc = "Create Horizontal Split ( Same Buffer )" })
--- split window vertically with same buffer
-key.set("n", "sv", "<Cmd>vsplit<CR>", opts, { desc = "Create Vertical Split ( Same Buffer )" })
+vim.keymap.set("i", "<C-h>", "<Left>", custom_opts)
+vim.keymap.set("i", "<C-l>", "<Right>", custom_opts)
+vim.keymap.set("i", "<C-j>", "<Down>", custom_opts)
+vim.keymap.set("i", "<C-k>", "<Up>", custom_opts)
--- nvim_test/nvim/lua/config/lazy.lua
+++ nvim_test/nvim/lua/config/lazy.lua
@@ -1,7 +1,3 @@
--- leader keys ( global and local )
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -18,42 +14,26 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 require("lazy").setup({
   spec = {
-    -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "github_dark_default" } },
-
-    { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.lang.clangd" },
-    { import = "lazyvim.plugins.extras.lang.git" },
-    { import = "lazyvim.plugins.extras.lang.java" },
-    { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.lang.sql" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.formatting.black" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-
-    -- import/override with your plugins
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "plugins" },
   },
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
+    version = false,
   },
+  -- install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
-    enabled = true, -- check for plugin updates periodically
-    notify = false, -- notify on update
-  }, -- automatically check for plugin updates
+    enabled = true,
+    notify = false,
+    frequency = 86400,
+  },
   performance = {
     rtp = {
-      -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
         "tarPlugin",
@@ -64,6 +44,8 @@ require("lazy").setup({
     },
   },
   ui = {
+    title = "Lazy Plugin Manager",
+    title_pos = "left",
     border = "rounded",
   },
 })
